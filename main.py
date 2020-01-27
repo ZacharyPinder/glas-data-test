524000b339d000ea4cc4072bf151e10a42452cdc
Zachary Pinder <zac.pinder@gmail.com>
2020-01-27 15:58:30 +0000

    Added first part of adding children to tree

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import json
 from num2words import num2words
 
 def parent_tree(tree, depth):
-    output ={}
+    output = {}
+    parents = []
+    in_tree = []
     if depth == 0:
         return(output)
     for item in tree:
         if item["parentId"] is None:
             i =+ 1
             output[num2words(i)] = {"id" : item["id"], "parentId" : None, "children" : []}
-    
-
-    print(output)
+            parents.append(item["id"])
+    for item in tree:
+        if item["parentId"] in parents:
+            i += 1
+            output[num2words(i)] = {"id" : item["id"], "parentId" : item["parentId"], "children" : []}
+            for item2 in output.items():
+                if item2[1]["id"] == item["parentId"]:
+                    item2[1]["children"].append(item["id"])
+    return(output)
             
 
 with open("senario/2.json") as file:
     data = json.load(file)
 
-parent_tree(data[0],data[1])
+tree = parent_tree(data[0],data[1])
+print(tree)
