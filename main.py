51d457fd36fd10db75ab2fd2942d5c05af2e5479
Zachary Pinder <zac.pinder@gmail.com>
2020-01-27 16:43:20 +0000

    Trying to add checks for bad data, checks do not currently work

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 from num2words import num2words
 
 def parent_tree(tree, depth):
+    if check_data(tree, depth) != 0:
+        print(check_data)
     output = {}
     parents = []
     in_tree = []
@@ -9,6 +11,7 @@ def parent_tree(tree, depth):
     
     if depth == 0:
         return(output)
+
     for item in tree:
         if item["parentId"] is None:
             i =+ 1
@@ -30,9 +33,26 @@ def parent_tree(tree, depth):
     
     return(output)
 
+def check_data(tree, depth):
+    print(len(tree))
+    all_items = []
+    for item in tree:
+        print(item["id"])
+        all_items.append(item["id"])
+        if item["id"] is not str:
+            return("Item id's must be strings")
+        if item["parentId"] is not str and item["parentId"] is not None:
+            return("parentId must be string or null")
+    for item in tree:
+        if item["parentId"] not in all_items:
+            print(item["parentId"])
+            return("One or more items have a parent that does not exist")
+    if type(depth) is not int:
+        return("Depth must be an interger")
+    return(0)
             
 
-with open("senario/2.json") as file:
+with open("senario/4.json") as file:
     data = json.load(file)
 
 tree = parent_tree(data[0],data[1])
