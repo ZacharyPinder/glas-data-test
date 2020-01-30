0f70b7ed37f8837972b047a1fc0a526066c0ae8c
Zachary Pinder <zac.pinder@gmail.com>
2020-01-30 15:50:44 +0000

    Removed non-functional code and started working on different solution

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import json
 from num2words import num2words
 
 def parent_tree(tree, depth):
-    if check_data(tree, depth) != 0:
-        print(check_data)
+    if type(depth) is not int:
+        return("Depth must be an int")
+    
     output = {}
     parents = []
     in_tree = []
+    all_items = []
     layers = 0
     
     if depth == 0:
         return(output)
-
+    
     for item in tree:
+        all_items.append(item["id"])
+        print(all_items)
         if item["parentId"] is None:
             i =+ 1
             output[num2words(i)] = {"id" : item["id"], "parentId" : None, "children" : []}
@@ -32,27 +36,9 @@ def parent_tree(tree, depth):
         layers += 1
     
     return(output)
-
-def check_data(tree, depth):
-    print(len(tree))
-    all_items = []
-    for item in tree:
-        print(item["id"])
-        all_items.append(item["id"])
-        if item["id"] is not str:
-            return("Item id's must be strings")
-        if item["parentId"] is not str and item["parentId"] is not None:
-            return("parentId must be string or null")
-    for item in tree:
-        if item["parentId"] not in all_items:
-            print(item["parentId"])
-            return("One or more items have a parent that does not exist")
-    if type(depth) is not int:
-        return("Depth must be an interger")
-    return(0)
             
 
-with open("senario/4.json") as file:
+with open("senario/1.json") as file:
     data = json.load(file)
 
 tree = parent_tree(data[0],data[1])
